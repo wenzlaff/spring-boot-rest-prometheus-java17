82bcbc7cfe3617fc12d37b2ecefccedb40f96e9d
Christoffer Vig <christoffer.vig@gmail.com>
2022-09-14 23:08:47 +0100

    fix: updated and copied from java11 version

---
+++ src/main/java/com/github/jenkinsx/quickstarts/springboot/rest/prometheus/RestPrometheusApplication.java
@@ -0,0 +1,31 @@
+package com.github.jenkinsx.quickstarts.springboot.rest.prometheus;
+
+import static java.util.Collections.singletonMap;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class RestPrometheusApplication {
+
+  @Autowired private MeterRegistry registry;
+
+  @GetMapping(path = "/", produces = "application/json")
+  @ResponseBody
+  public Map<String, Object> landingPage() {
+    Counter.builder("mymetric").tag("foo", "bar").register(registry).increment();
+    return singletonMap("hello", "world");
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestPrometheusApplication.class, args);
+  }
+}
---
+++ src/test/java/com/github/jenkinsx/quickstarts/springboot/rest/prometheus/RestPrometheusApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.jenkinsx.quickstarts.springboot.rest.prometheus;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class RestPrometheusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
